7129912f02e73bb6a4f1e13cda7efd7dec1e51d4
David Huber <david.huber@noaa.gov>
2025-11-13 16:20:46 +0000

    Trim down GDA module

--- GDA/gda/modulefiles/archive.wcoss2.lua
+++ GDA/gda/modulefiles/archive.wcoss2.lua
@@ -10,15 +10,7 @@ load(pathJoin("cmdaccel", os.getenv("cmdaccel_ver")))
 
 load(pathJoin("python", os.getenv("python_ver")))
 load(pathJoin("prod_envir", os.getenv("prod_envir_ver")))
-load(pathJoin("libjpeg", os.getenv("libjpeg_ver")))
 
-load(pathJoin("cdo", os.getenv("cdo_ver")))
-
-load(pathJoin("hdf5", os.getenv("hdf5_ver")))
-load(pathJoin("netcdf", os.getenv("netcdf_ver")))
-
-load(pathJoin("udunits", os.getenv("udunits_ver")))
-load(pathJoin("nco", os.getenv("nco_ver")))
 load(pathJoin("prod_util", os.getenv("prod_util_ver")))
 load(pathJoin("grib_util", os.getenv("grib_util_ver")))
 load(pathJoin("bufr_dump", os.getenv("bufr_dump_ver")))
